40df9e67ebc585b46ea5e4e08d8a3595810e282a
Nikita Biryukov <chppppr@yandex.ru>
2023-05-12 13:09:54 +0700

    wow sorting list

--- list.c
+++ list.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "list.h"
 
@@ -34,5 +35,31 @@ void list_traverse(List* list, void(*pfun)(char* str))
 	while (node != NULL) {
 		(*pfun)(str);
 		node = node->str;
+	}
+}
+
+int cmp(char* s1, char* s2)
+{
+	return strcmp(s1, s2) < 0;
+}
+
+void swap(List** l1, List** l2)
+{
+	List* temp = *l1;
+	*l1 = *l2;
+	*l2 = temp;
+}
+
+void list_sort(List* list)
+{
+	char* temp;
+	for (List* i = list; i != NULL; i = i->next) {
+		List* min = i;
+		while (List j = i->next; j != NULL; j = j->next) {
+			if (cmp(j->string, min)) {
+				min = j;
+			}
+		}
+		swap(i, min);
 	}
 }
